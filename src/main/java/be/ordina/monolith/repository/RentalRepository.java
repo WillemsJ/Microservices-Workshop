f1f44667aec6c7767eb4938c118efa5bf908ea41
Kevin Van Houtte <kevin.van.houtte1@gmail.com>
2017-09-07 21:27:30 +0200

    add solution chpt1

--- src/main/java/be/ordina/monolith/repository/RentalRepository.java
+++
@@ -1,9 +0,0 @@
-package be.ordina.monolith.repository;
-
-import be.ordina.monolith.model.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RentalRepository extends JpaRepository<Rental, UUID> {
-}
